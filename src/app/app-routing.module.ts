6beb15ea8565dbb812e87e7f4761c51062775d1f
william.luna <william.luna@rappi.com>
2023-12-18 17:28:35 -0500

    28 - Carga perezosa (Lazy loading)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path:'',
+      loadChildren: () => import('./welcome/welcome.module').then(m => m.WelcomeModule)
+    },
+    {
+      path: 'courses',
+      loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListCoursesComponent } from './pages/list-courses/list-courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: ListCoursesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
